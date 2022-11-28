2c9ebe1b91dd56a89cb94794ecbee174dbb2d4e8
kiattisak.rod <kiattisak.rod@KIATTISAK_ROD>
2022-11-28 11:04:53 +0700

    Add read file Async

--- Program.cs
+++ Program.cs
@@ -1,37 +1,40 @@
 ﻿
-await callMethod();
-Console.ReadKey();
 
-static async Task callMethod()
+Task task = new Task(CallMethod);
+task.Start();
+task.Wait();
+Console.ReadLine();
+
+
+static async void CallMethod()
 {
-    Method2();
-    var count = await Method1();
-    Method3(count);
+    string filePath = "D:\\sampleFile.txt";
+    Task<int> task = ReadFile(filePath);
+
+    Console.WriteLine(" Other Work 1");
+    Console.WriteLine(" Other Work 2");
+    Console.WriteLine(" Other Work 3");
+
+    int length = await task;
+    Console.WriteLine(" Total length: " + length);
+
+    Console.WriteLine(" After work 1");
+    Console.WriteLine(" After work 2");
 }
 
-static async Task<int> Method1()
+static async Task<int> ReadFile(string file)
 {
-    int count = 0;
-    await Task.Run(() =>
-    {
-        for (int i = 0; i < 100; i++)
-        {
-            Console.WriteLine(" Method 1");
-            count += 1;
-        }
-    });
-    return count;
-}
+    int length = 0;
 
-static void Method2()
-{
-    for (int i = 0; i < 25; i++)
+    Console.WriteLine(" File reading is stating");
+    using (StreamReader reader = new StreamReader(file))
     {
-        Console.WriteLine(" Method 2");
+        // Reads all characters from the current position to the end of the stream asynchronously
+        // and returns them as one string.
+        string s = await reader.ReadToEndAsync();
+
+        length = s.Length;
     }
-}
-
-static void Method3(int count)
-{
-    Console.WriteLine("Total count is " + count);
+    Console.WriteLine(" File reading is completed");
+    return length;
 }
