192cef2e967cc5df78c0069fbc7e0c3eb41c1884
kiattisak.rod <kiattisak.rod@KIATTISAK_ROD>
2022-11-28 10:11:10 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+Method1();
+Method2();
+Console.ReadKey();
+
+static async Task Method1()
+{
+    await Task.Run(() =>
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Console.WriteLine(" Method 1");
+            // Do something
+            Task.Delay(100).Wait();
+        }
+    });
+}
+
+
+static void Method2()
+{
+    for (int i = 0; i < 25; i++)
+    {
+        Console.WriteLine(" Method 2");
+        // Do something
+        Task.Delay(100).Wait();
+    }
+}
